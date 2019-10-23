0bd230c5a6eb6349890d6c06c1d854f1b8e8665a
shavros <shavrin.sb@phystech.edu>
2019-10-24 02:34:28 +0300

    Вроде как наипсал простейшее wsgi приложение

--- app_hw_3/my_app.py
+++ app_hw_3/my_app.py
@@ -1,5 +1,10 @@
 def handler(environ, start_response):
-    data = b'Hello, world'
+    age = 10
+    if age < 18:
+        data = b'You are very young yet'
+    elif age >= 18:
+        data = b'Oh, you are so adult'
     headers = [('Content-Type', 'text/plain'), ('Content-Length', str(len(data)))]
     start_response('200 OK', headers)
     return [data]
+
