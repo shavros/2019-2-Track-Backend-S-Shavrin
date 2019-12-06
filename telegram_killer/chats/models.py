f06da4e1fc8b0d665a45743f49f8600c085283ab
shavros <shavrin.sb@phystech.edu>
2019-12-06 11:49:57 +0300

    fixed hometask5

--- telegram_killer/chats/models.py
+++ telegram_killer/chats/models.py
@@ -4,22 +4,22 @@ from django.db import models
 
 
 class Chat(models.Model):
-    is_group_chat = models.BooleanField(default=False)
-    topic = models.CharField(max_length=16)
-    last_message = models.CharField(max_length=32, default='null')
+    is_group_chat = models.BooleanField(default=False, verbose_name='Групповой чат')
+    topic = models.CharField(max_length=16, verbose_name='Тема')
+    last_message = models.CharField(max_length=32, default='null', verbose_name='Последнее сообщение')
 
 
 class Message(models.Model):
-    chat = models.ForeignKey('Chat', on_delete=models.CASCADE)
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    content = models.TextField()
-    added_at = models.DateTimeField(null=False)
+    chat = models.ForeignKey('Chat', on_delete=models.CASCADE, verbose_name='id чата')
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='id пользователя')
+    content = models.TextField(verbose_name='Содержание сообщения')
+    added_at = models.DateTimeField(null=False, auto_now=True, verbose_name='Дата и время отправкивки')
 
 
 class Attachment(models.Model):
-    chat = models.ForeignKey('Chat', on_delete=models.CASCADE)
-    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
-    message = models.ForeignKey('Message', on_delete=models.CASCADE)
-    type = models.CharField(max_length=16)
-    url = models.URLField()
+    chat = models.ForeignKey('Chat', on_delete=models.CASCADE, verbose_name='id чата')
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, verbose_name='id ользователя')
+    message = models.ForeignKey('Message', on_delete=models.CASCADE, verbose_name='id сообщения')
+    type = models.CharField(max_length=16, verbose_name='Тип содержимого')
+    url = models.URLField(verbose_name='Ссылка на вложение')
 
--- telegram_killer/users/models.py
+++ telegram_killer/users/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(models.Model):
-    name = models.CharField(max_length=16)
-    avatar = models.ImageField()
-    nick = models.CharField(max_length=16)
+    name = models.CharField(max_length=16, verbose_name='Имя пользователя')
+    avatar = models.ImageField(verbose_name='Аватрака пользователя')
+    nick = models.CharField(max_length=16, verbose_name='Никнейм пользователя')
 
 
 class Member(models.Model):
-    user = models.ForeignKey('User', on_delete=models.CASCADE)
-    chat = models.ForeignKey('chats.Chat', on_delete=models.CASCADE)
-    new_messages = models.TextField()
-    last_read_message = models.ForeignKey('chats.Message', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name='id пользователя')
+    chat = models.ForeignKey('chats.Chat', on_delete=models.CASCADE, verbose_name='id чата')
+    new_messages = models.TextField(verbose_name='Новые сообщения')
+    last_read_message = models.ForeignKey('chats.Message', on_delete=models.CASCADE, verbose_name='Последнее прочитанное сообщение')
 
